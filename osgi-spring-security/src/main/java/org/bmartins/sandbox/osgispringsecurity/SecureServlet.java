f93aba6b0ec9c572f49caa56d65f479260f6236d
Bernardo Martins <bsamartins@gmail.com>
2013-11-22 16:47:12 +0000

    Added project

---
+++ osgi-spring-security/src/main/java/org/bmartins/sandbox/osgispringsecurity/SecureServlet.java
@@ -0,0 +1,9 @@
+package org.bmartins.sandbox.osgispringsecurity;
+
+import org.apache.felix.scr.annotations.sling.SlingServlet;
+
+@SlingServlet
+public class SecureServlet extends org.apache.sling.api.servlets.SlingAllMethodsServlet {
+	
+	
+}
