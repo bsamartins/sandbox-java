7e201b787a47d3b76c40dba2e746a915375e86fa
Bernardo Martins <bsamartins@gmail.com>
2015-03-04 16:59:02 +0000

    Added thymeleaf configuration

---
+++ spring-webflow/src/main/java/org/bmartins/sandbox/springwebflow/config/ThymeleafConfiguration.java
@@ -0,0 +1,7 @@
+package org.bmartins.sandbox.springwebflow.config;
+
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+
+public class ThymeleafConfiguration extends ThymeleafAutoConfiguration {
+	
+}
--- spring-webflow/src/main/java/org/bmartins/sandbox/springwebflow/config/WebConfiguration.java
+++ spring-webflow/src/main/java/org/bmartins/sandbox/springwebflow/config/WebConfiguration.java
@@ -2,9 +2,9 @@ package org.bmartins.sandbox.springwebflow.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-public class WebConfiguration {
-
+public class WebConfiguration extends WebMvcConfigurerAdapter {
 }
