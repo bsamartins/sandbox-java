0677aeff79a5bd441875d6da05c8f685d49e18f7
Bernardo Martins <bsamartins@gmail.com>
2015-03-05 11:40:00 +0000

    fixed resources issue

--- spring-webflow/src/main/java/org/bmartins/sandbox/springwebflow/config/WebConfiguration.java
+++ spring-webflow/src/main/java/org/bmartins/sandbox/springwebflow/config/WebConfiguration.java
@@ -1,10 +1,10 @@
 package org.bmartins.sandbox.springwebflow.config;
 
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-public class WebConfiguration extends WebMvcConfigurerAdapter {
+public class WebConfiguration extends WebMvcAutoConfiguration {	
 }
