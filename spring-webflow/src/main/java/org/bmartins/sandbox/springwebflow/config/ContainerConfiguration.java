2c63a983cb0a30de8f6100007ceaa61a224416f4
Bernardo Martins <bsamartins@gmail.com>
2015-03-16 14:30:54 +0000

    Reverted back to tomcat

--- spring-webflow/src/main/java/org/bmartins/sandbox/springwebflow/config/ContainerConfiguration.java
+++ spring-webflow/src/main/java/org/bmartins/sandbox/springwebflow/config/ContainerConfiguration.java
@@ -1,6 +1,5 @@
 package org.bmartins.sandbox.springwebflow.config;
 
-import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,14 +7,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ContainerConfiguration {
 	
-//	@Bean
-//	public TomcatEmbeddedServletContainerFactory tomcatEmbeddedServletContainerFactory() {
-//		return new TomcatEmbeddedServletContainerFactory();
-//	}
-
 	@Bean
-	public JettyEmbeddedServletContainerFactory jettyEmbeddedServletContainerFactory() {
-		return new JettyEmbeddedServletContainerFactory();
+	public TomcatEmbeddedServletContainerFactory tomcatEmbeddedServletContainerFactory() {
+		return new TomcatEmbeddedServletContainerFactory();
 	}
-
 }
