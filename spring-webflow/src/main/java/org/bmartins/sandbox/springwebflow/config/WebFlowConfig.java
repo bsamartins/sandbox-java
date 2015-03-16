407db744486185006734a7d02e17e7d12ae6bbd9
Bernardo Martins <bsamartins@gmail.com>
2015-03-16 16:21:03 +0000

    Added flow

--- spring-webflow/src/main/java/org/bmartins/sandbox/springwebflow/config/WebFlowConfig.java
+++ spring-webflow/src/main/java/org/bmartins/sandbox/springwebflow/config/WebFlowConfig.java
@@ -1,42 +1,55 @@
 package org.bmartins.sandbox.springwebflow.config;
 
+import java.util.Arrays;
+
+import org.bmartins.sandbox.springwebflow.flows.simplescopeflow.SimpleScopeFlowHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.webflow.config.AbstractFlowConfiguration;
+import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
+import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
+import org.springframework.webflow.mvc.builder.MvcViewFactoryCreator;
 
 @Configuration
-public class WebFlowConfig {	
+public class WebFlowConfig extends AbstractFlowConfiguration {	
 	
 	@Autowired
 	private WebConfiguration webMvcConfig;
 	
-//	@Bean
-//	public FlowDefinitionRegistry flowRegistry(ServletContextTemplateResolver templateResolver) {
-//		return getFlowDefinitionRegistryBuilder(flowBuilderServices(templateResolver))
-//				.setBasePath("/WEB-INF")
-//				.addFlowLocationPattern("/**/*-flow.xml").build();
-//	}
+	@Bean
+	public FlowDefinitionRegistry flowRegistry() {
+		return getFlowDefinitionRegistryBuilder(flowBuilderServices())
+				.setBasePath("classpath:templates")
+				.addFlowLocationPattern("/**/*-flow.xml").build();
+	}
 	
-//	@Bean
-//	public FlowBuilderServices flowBuilderServices(ServletContextTemplateResolver templateResolver) {
-//		return getFlowBuilderServicesBuilder()
-//				.setViewFactoryCreator(mvcViewFactoryCreator())
-//				.setValidator(validator())
-//				.setDevelopmentMode(true)
-//				.build();
-//	}
+	@Bean
+	public FlowBuilderServices flowBuilderServices() {
+		return getFlowBuilderServicesBuilder()
+				.setViewFactoryCreator(mvcViewFactoryCreator())
+				.setValidator(validator())
+				.setDevelopmentMode(true)
+				.build();
+	}
 	
-//	@Bean
-//	public MvcViewFactoryCreator mvcViewFactoryCreator() {
-//		MvcViewFactoryCreator factoryCreator = new MvcViewFactoryCreator();
-//		factoryCreator.setViewResolvers(Arrays.<ViewResolver>asList(webMvcConfig.tilesViewResolver()));
-//		factoryCreator.setUseSpringBeanBinding(true);
-//		return factoryCreator;
-//	}
+	@Bean
+	public MvcViewFactoryCreator mvcViewFactoryCreator() {
+		MvcViewFactoryCreator factoryCreator = new MvcViewFactoryCreator();
+		factoryCreator.setViewResolvers(Arrays.<ViewResolver>asList(webMvcConfig.tilesViewResolver()));
+		factoryCreator.setUseSpringBeanBinding(true);
+		return factoryCreator;
+	}
 	
 	@Bean
 	public LocalValidatorFactoryBean validator() {
 		return new LocalValidatorFactoryBean();
+	}		
+	
+	@Bean(name="flows/simpleScope")
+	public SimpleScopeFlowHandler simpleScopeFlowHandler() {
+		return new SimpleScopeFlowHandler();
 	}
 }
---
+++ spring-webflow/src/main/java/org/bmartins/sandbox/springwebflow/flows/simplescopeflow/SimpleScopeFlowHandler.java
@@ -0,0 +1,29 @@
+package org.bmartins.sandbox.springwebflow.flows.simplescopeflow;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.webflow.core.FlowException;
+import org.springframework.webflow.execution.FlowExecutionOutcome;
+import org.springframework.webflow.execution.repository.NoSuchFlowExecutionException;
+import org.springframework.webflow.mvc.servlet.AbstractFlowHandler;
+
+public class SimpleScopeFlowHandler extends AbstractFlowHandler {
+    private static final String DEFAULT_URL = "/flows/simpleScope";
+
+    @Override
+    public String handleExecutionOutcome(FlowExecutionOutcome outcome, HttpServletRequest request,
+	    HttpServletResponse response) {
+    	return DEFAULT_URL;
+    }
+
+    @Override
+    public String handleException(FlowException e, HttpServletRequest request, HttpServletResponse response) {
+    	if (e instanceof NoSuchFlowExecutionException) {
+    		return DEFAULT_URL;
+    	} else {
+    		throw e;
+    	}
+    }
+
+}
