509dd863d431c753736ae73cde0720e06b828cad
Bernardo Martins <bsamartins@gmail.com>
2015-04-20 10:20:08 +0100

    non working java agent

--- java-agent/src/main/java/org/bmartins/sandbox/javaagent/FindUsageTransformer.java
+++ java-agent/src/main/java/org/bmartins/sandbox/javaagent/FindUsageTransformer.java
@@ -7,8 +7,14 @@ import java.security.ProtectionDomain;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
+import javassist.Modifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FindUsageTransformer implements ClassFileTransformer {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(FindUsageTransformer.class);
 	
 	Class clazz = null;
 	
@@ -19,9 +25,9 @@ public class FindUsageTransformer implements ClassFileTransformer {
     		Class<?> classBeingRedefined,  
     		ProtectionDomain protectionDomain,
             byte[] classfileBuffer)    throws IllegalClassFormatException {		
-        if(className.startsWith("org/bmartins")){
-        	System.out.println("Transform:" + className);
-            doClass(className, classBeingRedefined, classfileBuffer);
+        if(className.startsWith("org/bmartins/sandbox/springaspectj")){
+        	LOG.debug("Transform: {}", className);
+       		doClass(className, classBeingRedefined, classfileBuffer);
         }
         return classfileBuffer;
     }
@@ -29,15 +35,27 @@ public class FindUsageTransformer implements ClassFileTransformer {
     private byte[] doClass(String name, Class clazz, byte[] b) {
     	ClassPool pool = ClassPool.getDefault();
         CtClass cl = null;
+        CtMethod currentMethod = null;     
         try {
-        	cl = pool.makeClass(new java.io.ByteArrayInputStream(b));
+        	cl = pool.makeClass(new java.io.ByteArrayInputStream(b));        	
         	for(CtMethod method: cl.getMethods()) {
-            	// here you have lot of options to explore
-            	method.insertBefore("System.out.println(Thread.currentThread().getStackTrace()[0].getClassName()+ Thread.currentThread().getStackTrace()[0].getMethodName());");        		
+        		currentMethod = method;
+        		if(!method.isEmpty() 
+        				&& !Modifier.isNative(method.getModifiers())) {
+                	LOG.debug(method.getLongName());
+                	// here you have lot of options t	o explore        		
+                	method.insertBefore("System.out.println(Thread.currentThread().getStackTrace()[0].getClassName()+ Thread.currentThread().getStackTrace()[0].getMethodName());");
+        		} else {
+        			LOG.debug("Skipping: {}", method.getLongName());
+        		}
         	}
         	b = cl.toBytecode();
         } catch (Exception e) {
-        	System.err.println("Could not instrument  " + name + ",  exception : " + e.getMessage());
+        	if(currentMethod == null) {
+        		LOG.error("Could not instrument {}: {}", name, e.getMessage());
+        	} else {
+        		LOG.error("Could not instrument {}, method {}: {}", name, currentMethod.getSignature(), e.getMessage());
+        	}        	
         } finally {
         	if (cl != null) {
         		cl.detach();
